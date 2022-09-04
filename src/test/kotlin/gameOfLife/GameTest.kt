308cee53b7d204e2c948e44a85a9254c2c627a66
Sol Turipe <solcis.bio@gmail.com>
2022-09-04 21:10:04 +0200

    game test

--- src/test/kotlin/gameOfLife/GameTest.kt
+++ src/test/kotlin/gameOfLife/GameTest.kt
@@ -1,4 +1,13 @@
 package gameOfLife
 
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
 class GameTest {
+
+    @Test
+    fun `game should be generated with given size`() {
+        val game = Game(size = 3)
+        assertEquals(9, game.cells.size)
+    }
 }
